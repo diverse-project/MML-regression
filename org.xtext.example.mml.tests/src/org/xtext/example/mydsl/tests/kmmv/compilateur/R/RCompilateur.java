9d5bae34178bbc62814b8c912ac3a2684f74327b
Myriam Pavy <myriam.pavy@audencia.com>
2020-01-08 02:07:21 +0100

    Adding R compilator (v1)

--- org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/kmmv/compilateur/R/RCompilateur.java
+++ org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/kmmv/compilateur/R/RCompilateur.java
@@ -10,20 +10,112 @@ public class RCompilateur implements Compilateur {
 
 	@Override
 	public String compile(DataInput input, MLChoiceAlgorithm algorithm, RFormula formula, Validation validation, int uniqueId) {
-		// TODO Auto-generated method stub
-		return null;
+		Pair<List<String>, List<String>> inputC = compileDataInput(input);
+		Pair<List<String>, List<String>> formulaC = compileRFormula(formula);
+		Pair<List<String>, List<String>> validationC = ValidationCompiler.compile(validation);
+		Pair<List<String>, List<String>> algorithmC = MLAlgorithmCompiler.compile(algorithm.getAlgorithm(), validation);
+		Pair<List<String>, List<String>> validationMetricsC = ValidationCompiler.compileMetrics(validation);
+		
+		List<String> buffer = new LinkedList<>(), bufferImports = new LinkedList<>();
+		
+		bufferImports.addAll(inputC.getFirst());
+		bufferImports.addAll(formulaC.getFirst());
+		bufferImports.addAll(validationC.getFirst());
+		bufferImports.addAll(algorithmC.getFirst());
+		bufferImports.addAll(validationMetricsC.getFirst());
+		
+		buffer.addAll(filterImport(bufferImports));
+		buffer.add("");
+		buffer.addAll(inputC.getSecond());
+		buffer.addAll(formulaC.getSecond());
+		buffer.addAll(validationC.getSecond());
+		buffer.addAll(algorithmC.getSecond());
+		buffer.addAll(validationMetricsC.getSecond());
+		
+		return String.join("\n", buffer);
 	}
 
 	@Override
 	public String fileName(DataInput input, MLChoiceAlgorithm algorithm, Validation validation, int uniqueId) {
-		// TODO Auto-generated method stub
-		return null;
+		return String.format("%s_%s_%s_%s.R", input.getFilelocation().replace('.', '_'), Utils.algorithmName(algorithm.getAlgorithm()), Utils.stratificationToString(validation.getStratification()), uniqueId);
 	}
-
+	
+	private List<String> filterImport(List<String> imports) {
+		List<String> result = new LinkedList<>();
+		
+		for(String imp : imports) {
+			if(!result.contains(imp))
+				result.add(imp);
+		}
+		
+		return result;
+	}
+	
+	private Pair<List<String>, List<String>> compileDataInput(DataInput input) {
+		List<String> import_ = new LinkedList<>();
+		List<String> code_ = new LinkedList<>();
+		
+		CSVParsingConfiguration parsingInstruction = input.getParsingInstruction();
+		String separateur = parsingInstruction != null ? parsingInstruction.getSep().toString() : ",";
+		
+		code_.add(String.format("mml_data <- read.csv(file='%s' header=TRUE,sep='%s')", input.getFilelocation(), separateur));
+		
+		return new Pair<>(import_, code_);
+	}
+	
+	private Pair<List<String>, List<String>> compileRFormula(RFormula formula) {
+		List<String> import_ = new LinkedList<>();
+		List<String> code_ = new LinkedList<>();
+				
+		if(formula != null && formula.getPredictive() != null) {
+			if(formula.getPredictive().getColName() != null)
+				code_.add(String.format("Y_name <- '%s'", formula.getPredictive().getColName()));
+			else
+				code_.add(String.format("Y_name <- names(mml_data)[%d]", formula.getPredictive().getColumn()));
+		} else {
+			code_.add("Y_name <- names(mml_data)[ncol(mml_data)]");
+		}
+		code_.add("Y <- mml_data[Y_name][[1]]");
+		
+		if(formula != null && formula.getPredictors() != null) {
+			if(formula.getPredictors() instanceof AllVariables) {
+				code_.add("X <- mml_data[, !names(mml_data) %in% Y_name, drop = F]");
+			} else {
+				List<String> predictorsName = ((PredictorVariables) formula.getPredictors())
+						.getVars()
+						.stream()
+						.filter(variable -> variable.getColName() != null && !variable.getColName().isBlank())
+						.map(variable -> String.format("'%s'", variable.getColName()))
+						.collect(Collectors.toList());
+				List<Integer> predictorsIndex = ((PredictorVariables) formula.getPredictors())
+						.getVars()
+						.stream()
+						.filter(variable -> variable.getColName() == null || variable.getColName().isBlank())
+						.map(variable -> variable.getColumn())
+						.collect(Collectors.toList());
+				
+				code_.add("predictorsSet <- c()");
+				for(String predN : predictorsName)
+					code_.add(String.format("predictorsSet <- c(predictorsSet, %s)", predN));
+				for(Integer predI : predictorsIndex)
+					code_.add(String.format("predictorsSet <- c(predictorsSet, names(mml_data)[%d])", predI));
+				code_.add("predictorsSet <- unique(predictorsSet)");
+				code_.add("X <- mml_data[ , predictorsSet]");
+			}
+				
+		} else {
+			code_.add("X <- mml_data[, !names(mml_data) %in% Y_name, drop = F]");
+		}
+		
+		code_.add("X_names <- c(names(X))");
+		code_.add("formula <- as.formula(paste(Y_name, paste(X_names, collapse=' + '), sep' ~ '))")
+		
+		return new Pair<>(import_, code_);
+	}
+		
 	@Override
 	public String commandLine(String file) {
-		// TODO Auto-generated method stub
-		return null;
+		return String.format("R -q --vanilla < %s", file);
 	}
 
 }
---
+++ org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/kmmv/compilateur/R/ValidationCompiler.java
@@ -0,0 +1,123 @@
+package org.xtext.example.mydsl.tests.kmmv.compilateur.R;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.xtext.example.mydsl.mml.CrossValidation;
+import org.xtext.example.mydsl.mml.TrainingTest;
+import org.xtext.example.mydsl.mml.Validation;
+import org.xtext.example.mydsl.mml.ValidationMetric;
+import org.xtext.example.mydsl.tests.kmmv.compilateur.Pair;
+
+public class ValidationCompiler {
+	public static Pair<List<String>, List<String>> compile(Validation validation) {
+		if(validation.getStratification() instanceof CrossValidation)
+			return compile((CrossValidation)validation.getStratification(), validation.getMetric());
+		if(validation.getStratification() instanceof TrainingTest)
+			return compile((TrainingTest)validation.getStratification(), validation.getMetric());
+		return new Pair<>(List.of(), List.of());
+	}
+	
+	private static Pair<List<String>, List<String>> compile(CrossValidation stratificationMethod, List<ValidationMetric> metrics) {
+		List<String> import_ = new LinkedList<>();
+		List<String> code_ = new LinkedList<>();
+		
+		import_.add("library(caret)");
+		
+		if(metrics != null) {
+			int cv = 5;
+			if(stratificationMethod.getNumber() != 0)
+				cv = stratificationMethod.getNumber();
+			
+			if(metrics.contains("MAPE")) {
+				code_.add("mapeSummary <- function (data, lev = NULL, model = NULL) {");
+				code_.add(String.format("%smape <- function(a, b) mean(abs((a - b)/a))*100", Utils.tab()));
+				code_.add(String.format("%sout <- mape(data$obs, data$pred)", Utils.tab()));
+				code_.add(String.format("%snames(out) <- 'MAPE'", Utils.tab()));
+				code_.add(String.format("%sout", Utils.tab()));
+				code_.add("}");
+				
+				code_.add(String.format("train_control_mape <- trainControl(method='cv', number=%d, summaryFunction = mapeSummary)", cv));
+				
+				if(metrics.size()>1)
+					code_.add(String.format("train_control <- trainControl(method='cv', number=%d)", cv));
+			} else {
+				code_.add(String.format("train_control <- trainControl(method='cv', number=%d)", cv));
+			}
+		}
+		
+		return new Pair<>(import_, code_);
+	}
+	
+	private static Pair<List<String>, List<String>> compile(TrainingTest stratificationMethod, List<ValidationMetric> metrics) {
+		List<String> import_ = new LinkedList<>();
+		List<String> code_ = new LinkedList<>();
+		
+		if(stratificationMethod.getNumber() > 0 && stratificationMethod.getNumber() < 100)
+			code_.add(String.format("training_size = %f",stratificationMethod.getNumber()/100.0));
+		else
+			code_.add("training_size = 0.7");
+		code_.add("index <- createDataPartition(y = Y, p = training_size, list = F)");
+		code_.add("training <- mml_data[index, ]");
+		code_.add("testing <- mml_data[-index, ]");
+		
+		return new Pair<>(import_, code_);
+	}
+	
+	public static Pair<List<String>, List<String>> compileMetrics(Validation validation) {
+		if(validation.getStratification() instanceof CrossValidation)
+			return compile((CrossValidation)validation.getStratification(), validation.getMetric());
+		if(validation.getStratification() instanceof TrainingTest)
+			return compile((TrainingTest)validation.getStratification(), validation.getMetric());
+		return new Pair<>(List.of(), List.of());
+	}
+	
+	private static Pair<List<String>, List<String>> compileMetrics(CrossValidation stratificationMethod, List<ValidationMetric> metrics){
+		List<String> import_ = new LinkedList<>();
+		List<String> code_ = new LinkedList<>();
+		
+		if(metrics != null) {
+			for(ValidationMetric metric : metrics) {
+				switch(metric) {
+				case MAE:
+					code_.add("mean(model$resample$MAE)");
+					break;
+				case MSE:
+					code_.add("mean(model$resample$RMSE^2)");
+					break;
+				case MAPE:
+					code_.add("mean(model_mape$resample$MAPE)");
+					break;
+				}
+			}
+		}
+		
+		return new Pair<>(import_, code_);
+	}
+	
+	private static Pair<List<String>, List<String>> compileMetrics(TrainingTest stratificationMethod, List<ValidationMetric> metrics) {
+		List<String> import_ = new LinkedList<>();
+		List<String> code_ = new LinkedList<>();
+		
+		code_.add("Y_test <- testing[Y_name][[1]]");
+		
+		if(metrics != null) {
+			for(ValidationMetric metric : metrics) {
+				switch(metric) {
+				case MAE:
+					code_.add("mean(abs(preds - Y_test))");
+					break;
+				case MSE:
+					code_.add("mean((preds - Y_test)^2)");
+					break;
+				case MAPE:
+					code_.add("mean(abs((Y_test-preds)/Y_test) * 100)");
+					break;
+				}
+			}
+		}
+		
+		return new Pair<>(import_, code_);
+	}
+	
+}
