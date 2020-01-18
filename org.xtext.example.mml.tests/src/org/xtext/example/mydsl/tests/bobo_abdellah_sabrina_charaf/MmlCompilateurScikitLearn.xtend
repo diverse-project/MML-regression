99904f27f61b3520cebde65ce7eb14890b392fb6
barry <barrybobo55@gmail.com>
2020-01-18 19:14:22 +0100

    tests

--- org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/bobo_abdellah_sabrina_charaf/MmlCompilateurScikitLearn.xtend
+++ org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/bobo_abdellah_sabrina_charaf/MmlCompilateurScikitLearn.xtend
@@ -1,8 +1,183 @@
 package org.xtext.example.mydsl.tests.bobo_abdellah_sabrina_charaf
 
+import com.google.common.io.Files
+import com.google.inject.Inject
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.util.List
+import org.eclipse.xtext.testing.InjectWith
+import org.eclipse.xtext.testing.extensions.InjectionExtension
+import org.eclipse.xtext.testing.util.ParseHelper
+import org.junit.jupiter.api.^extension.ExtendWith
+import org.xtext.example.mydsl.mml.AllVariables
+import org.xtext.example.mydsl.mml.DT
+import org.xtext.example.mydsl.mml.DataInput
+import org.xtext.example.mydsl.mml.FormulaItem
+import org.xtext.example.mydsl.mml.MLChoiceAlgorithm
+import org.xtext.example.mydsl.mml.MMLModel
+import org.xtext.example.mydsl.mml.PredictorVariables
+import org.xtext.example.mydsl.mml.RFormula
+import org.xtext.example.mydsl.mml.RandomForest
+import org.xtext.example.mydsl.mml.StratificationMethod
+import org.xtext.example.mydsl.mml.Validation
+import org.xtext.example.mydsl.mml.ValidationMetric
+import org.xtext.example.mydsl.mml.XFormula
+import org.xtext.example.mydsl.tests.MmlInjectorProvider
+
+@ExtendWith(InjectionExtension)
+@InjectWith(MmlInjectorProvider)
 class MmlCompilateurScikitLearn {
 	
 	def static void main(String[] args) {
 		
 	}
+	
+	@Inject
+	ParseHelper<MMLModel> parseHelper
+	
+	
+	
+	def String compileDataInput(CharSequence code) {
+		val MMLModel result = parseHelper.parse(
+			code
+		)
+		
+		val DataInput dataInput = result.input;
+		val String fileLocation = dataInput.filelocation;
+		
+		//Algorithm
+		val List<MLChoiceAlgorithm> mlChoiceAlgorithms = result.algorithms;
+		var String algorithmImport="";
+		var String algorithmBody="";
+		
+		for(MLChoiceAlgorithm mlAlgorithm: mlChoiceAlgorithms){
+			switch mlAlgorithm {
+				DT: {
+					algorithmImport += "\nfrom sklearn import tree";
+					algorithmBody += "\nclf = tree.DecisionTreeRegressor()";
+					algorithmBody += "\nclf.fit(X_train, y_train)";
+					algorithmBody += "\ny_pred =  clf.predict(X_test)"
+				
+				}
+				
+				RandomForest: {
+					algorithmImport += "\nimport numpy as np";
+					algorithmImport += "\nfrom sklearn.ensemble import RandomForestRegressor";
+					algorithmBody += "\nregressor = RandomForestRegressor()";
+					algorithmBody += "\nregressor.fit(X_train, y_train)";
+					algorithmBody += "\ny_pred = regressor.predict(X_test)"
+				}
+				default : print("default")
+			
+			}
+		}
+		
+		
+		//TrainningTest
+		val Validation validation = result.validation;
+		val StratificationMethod stratification = validation.stratification;
+		//Metric
+		val List<ValidationMetric> metrics = validation.metric;
+		
+		//TrainningPercentage
+		val double percentageTraining = stratification.number as double / 100.0;
+		val double percentageTest = 1.0 - percentageTraining;
+		
+		val String trainning = "train_test_split";
+		
+		// start of Python generation
+		var String pythonImport = "import pandas as pd\n";
+		pythonImport += algorithmImport+"\n";
+		
+		for(ValidationMetric validationMetric: metrics){
+			pythonImport += "from sklearn.metrics import "+validationMetric.literal.toString()+"\n";
+		}
+		pythonImport += "from sklearn.model_selection import "+trainning+"\n";
+		
+		val String	csvReading = "\nmml_data = pd.read_csv(\""+ fileLocation + "\")";
+		var String	pandasCode = pythonImport + csvReading;
+		
+		//Formula
+		val RFormula formula = result.formula;
+		if (formula === null) {
+			var String column= "\ncolumn = mml_data.columns[-1]";
+			pandasCode += "\n"+column+" \nX = mml_data.drop(columns=[column]) ";
+			pandasCode += "\ny = mml_data[column] ";
+		} else {
+			
+			var int predictiveColumn
+			var String predictiveColName
+			if (formula.predictive !== null) {
+				val FormulaItem predictive = formula.predictive;
+				println("predictive = "+(predictive));
+				
+				if(predictive.column !== 0 ){
+					predictiveColumn = predictive.column;
+					pandasCode += "y = mml_data.iloc[:, "+predictiveColumn+"].values";
+				}
+				else if(predictive.colName !== null){
+					predictiveColName = formula.predictive.colName;
+					pandasCode += "\ny = mml_data['"+predictiveColName+"'] ";
+				}
+				
+			}
+			else{
+				print("Here")
+				var String column= "\ncolumn = mml_data.columns[-1]";
+				pandasCode += "\n"+column;
+				pandasCode += "\ny = mml_data[column] ";
+			}
+			val XFormula xformula = formula.predictors;
+			switch xformula{
+				AllVariables:{
+					if (predictiveColumn !== 0){
+						pandasCode += "X = mml_data.iloc[:, 0:"+predictiveColumn+"].values";
+					}else if(predictiveColName !== null){
+						pandasCode += "\ncolumn = \""+predictiveColName+"\"";
+						pandasCode += "\nX = mml_data.drop(columns=[column]) ";
+					}
+					else{
+						var String column= "\ncolumn = mml_data.columns[-1]";
+						pandasCode += "\n"+column+" \nX = mml_data.drop(columns=[column]) ";
+					}
+				}
+				PredictorVariables:{
+					var PredictorVariables	predictorsVariables =  formula.predictors as PredictorVariables;
+					val List<FormulaItem> predictorsList = predictorsVariables.vars
+				}	
+				default : print("default")	
+			}
+		}
+		pandasCode += "\ntest_size = "+ percentageTest ;
+		pandasCode += "\nX_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size) ";
+		pandasCode += "\n"+algorithmBody;
+		
+		for(ValidationMetric validationMetric: metrics){
+			pandasCode += "\naccuracy = "+validationMetric.literal.toString()+"(y_test, y_pred)";
+			pandasCode += "\nprint('"+validationMetric.literal.toString()+":', accuracy)";
+		}
+				
+		Files.write(pandasCode.getBytes(), new File("mml.py"));
+		// end of Python generation
+		
+		/*
+		 * Calling generated Python script (basic solution through systems call)
+		 * we assume that "python" is in the path
+		 * 
+		 * virtualenv -p python3 venv
+		 * 
+		 * venv/bin/pip install -r requirements.txt
+		 */
+		
+		val Process	p = Runtime.getRuntime().exec("python mml.py");
+		val BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		var String line;
+		
+		while (( line = in.readLine()) !== null) {
+			System.out.println(line);
+		}
+		return pandasCode;
+	}
+	
 }
--- org.xtext.example.mml.tests/xtend-gen/org/xtext/example/mydsl/tests/bobo_abdellah_sabrina_charaf/MmlCompilateurScikitLearn.java
+++ org.xtext.example.mml.tests/xtend-gen/org/xtext/example/mydsl/tests/bobo_abdellah_sabrina_charaf/MmlCompilateurScikitLearn.java
@@ -1,7 +1,208 @@
 package org.xtext.example.mydsl.tests.bobo_abdellah_sabrina_charaf;
 
+import com.google.common.io.Files;
+import com.google.inject.Inject;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.extensions.InjectionExtension;
+import org.eclipse.xtext.testing.util.ParseHelper;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.InputOutput;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.xtext.example.mydsl.mml.AllVariables;
+import org.xtext.example.mydsl.mml.DT;
+import org.xtext.example.mydsl.mml.DataInput;
+import org.xtext.example.mydsl.mml.FormulaItem;
+import org.xtext.example.mydsl.mml.MLChoiceAlgorithm;
+import org.xtext.example.mydsl.mml.MMLModel;
+import org.xtext.example.mydsl.mml.PredictorVariables;
+import org.xtext.example.mydsl.mml.RFormula;
+import org.xtext.example.mydsl.mml.RandomForest;
+import org.xtext.example.mydsl.mml.StratificationMethod;
+import org.xtext.example.mydsl.mml.Validation;
+import org.xtext.example.mydsl.mml.ValidationMetric;
+import org.xtext.example.mydsl.mml.XFormula;
+import org.xtext.example.mydsl.tests.MmlInjectorProvider;
+
+@ExtendWith(InjectionExtension.class)
+@InjectWith(MmlInjectorProvider.class)
 @SuppressWarnings("all")
 public class MmlCompilateurScikitLearn {
   public static void main(final String[] args) {
+  }
+  
+  @Inject
+  private ParseHelper<MMLModel> parseHelper;
+  
+  public String compileDataInput(final CharSequence code) {
+    try {
+      final MMLModel result = this.parseHelper.parse(code);
+      final DataInput dataInput = result.getInput();
+      final String fileLocation = dataInput.getFilelocation();
+      final List<MLChoiceAlgorithm> mlChoiceAlgorithms = result.getAlgorithms();
+      String algorithmImport = "";
+      String algorithmBody = "";
+      for (final MLChoiceAlgorithm mlAlgorithm : mlChoiceAlgorithms) {
+        boolean _matched = false;
+        if (mlAlgorithm instanceof DT) {
+          _matched=true;
+          String _algorithmImport = algorithmImport;
+          algorithmImport = (_algorithmImport + "\nfrom sklearn import tree");
+          String _algorithmBody = algorithmBody;
+          algorithmBody = (_algorithmBody + "\nclf = tree.DecisionTreeRegressor()");
+          String _algorithmBody_1 = algorithmBody;
+          algorithmBody = (_algorithmBody_1 + "\nclf.fit(X_train, y_train)");
+          String _algorithmBody_2 = algorithmBody;
+          algorithmBody = (_algorithmBody_2 + "\ny_pred =  clf.predict(X_test)");
+        }
+        if (!_matched) {
+          if (mlAlgorithm instanceof RandomForest) {
+            _matched=true;
+            String _algorithmImport = algorithmImport;
+            algorithmImport = (_algorithmImport + "\nimport numpy as np");
+            String _algorithmImport_1 = algorithmImport;
+            algorithmImport = (_algorithmImport_1 + "\nfrom sklearn.ensemble import RandomForestRegressor");
+            String _algorithmBody = algorithmBody;
+            algorithmBody = (_algorithmBody + "\nregressor = RandomForestRegressor()");
+            String _algorithmBody_1 = algorithmBody;
+            algorithmBody = (_algorithmBody_1 + "\nregressor.fit(X_train, y_train)");
+            String _algorithmBody_2 = algorithmBody;
+            algorithmBody = (_algorithmBody_2 + "\ny_pred = regressor.predict(X_test)");
+          }
+        }
+        if (!_matched) {
+          InputOutput.<String>print("default");
+        }
+      }
+      final Validation validation = result.getValidation();
+      final StratificationMethod stratification = validation.getStratification();
+      final List<ValidationMetric> metrics = validation.getMetric();
+      int _number = stratification.getNumber();
+      final double percentageTraining = (((double) _number) / 100.0);
+      final double percentageTest = (1.0 - percentageTraining);
+      final String trainning = "train_test_split";
+      String pythonImport = "import pandas as pd\n";
+      String _pythonImport = pythonImport;
+      pythonImport = (_pythonImport + (algorithmImport + "\n"));
+      for (final ValidationMetric validationMetric : metrics) {
+        String _pythonImport_1 = pythonImport;
+        String _string = validationMetric.getLiteral().toString();
+        String _plus = ("from sklearn.metrics import " + _string);
+        String _plus_1 = (_plus + "\n");
+        pythonImport = (_pythonImport_1 + _plus_1);
+      }
+      String _pythonImport_2 = pythonImport;
+      pythonImport = (_pythonImport_2 + (("from sklearn.model_selection import " + trainning) + "\n"));
+      final String csvReading = (("\nmml_data = pd.read_csv(\"" + fileLocation) + "\")");
+      String pandasCode = (pythonImport + csvReading);
+      final RFormula formula = result.getFormula();
+      if ((formula == null)) {
+        String column = "\ncolumn = mml_data.columns[-1]";
+        String _pandasCode = pandasCode;
+        pandasCode = (_pandasCode + (("\n" + column) + " \nX = mml_data.drop(columns=[column]) "));
+        String _pandasCode_1 = pandasCode;
+        pandasCode = (_pandasCode_1 + "\ny = mml_data[column] ");
+      } else {
+        int predictiveColumn = 0;
+        String predictiveColName = null;
+        FormulaItem _predictive = formula.getPredictive();
+        boolean _tripleNotEquals = (_predictive != null);
+        if (_tripleNotEquals) {
+          final FormulaItem predictive = formula.getPredictive();
+          InputOutput.<String>println(("predictive = " + predictive));
+          int _column = predictive.getColumn();
+          boolean _tripleNotEquals_1 = (_column != 0);
+          if (_tripleNotEquals_1) {
+            predictiveColumn = predictive.getColumn();
+            String _pandasCode_2 = pandasCode;
+            pandasCode = (_pandasCode_2 + (("y = mml_data.iloc[:, " + Integer.valueOf(predictiveColumn)) + "].values"));
+          } else {
+            String _colName = predictive.getColName();
+            boolean _tripleNotEquals_2 = (_colName != null);
+            if (_tripleNotEquals_2) {
+              predictiveColName = formula.getPredictive().getColName();
+              String _pandasCode_3 = pandasCode;
+              pandasCode = (_pandasCode_3 + (("\ny = mml_data[\'" + predictiveColName) + "\'] "));
+            }
+          }
+        } else {
+          InputOutput.<String>print("Here");
+          String column_1 = "\ncolumn = mml_data.columns[-1]";
+          String _pandasCode_4 = pandasCode;
+          pandasCode = (_pandasCode_4 + ("\n" + column_1));
+          String _pandasCode_5 = pandasCode;
+          pandasCode = (_pandasCode_5 + "\ny = mml_data[column] ");
+        }
+        final XFormula xformula = formula.getPredictors();
+        boolean _matched_1 = false;
+        if (xformula instanceof AllVariables) {
+          _matched_1=true;
+          if ((predictiveColumn != 0)) {
+            String _pandasCode_6 = pandasCode;
+            pandasCode = (_pandasCode_6 + (("X = mml_data.iloc[:, 0:" + Integer.valueOf(predictiveColumn)) + "].values"));
+          } else {
+            if ((predictiveColName != null)) {
+              String _pandasCode_7 = pandasCode;
+              pandasCode = (_pandasCode_7 + (("\ncolumn = \"" + predictiveColName) + "\""));
+              String _pandasCode_8 = pandasCode;
+              pandasCode = (_pandasCode_8 + "\nX = mml_data.drop(columns=[column]) ");
+            } else {
+              String column_2 = "\ncolumn = mml_data.columns[-1]";
+              String _pandasCode_9 = pandasCode;
+              pandasCode = (_pandasCode_9 + (("\n" + column_2) + " \nX = mml_data.drop(columns=[column]) "));
+            }
+          }
+        }
+        if (!_matched_1) {
+          if (xformula instanceof PredictorVariables) {
+            _matched_1=true;
+            XFormula _predictors = formula.getPredictors();
+            PredictorVariables predictorsVariables = ((PredictorVariables) _predictors);
+            final List<FormulaItem> predictorsList = predictorsVariables.getVars();
+          }
+        }
+        if (!_matched_1) {
+          InputOutput.<String>print("default");
+        }
+      }
+      String _pandasCode_6 = pandasCode;
+      pandasCode = (_pandasCode_6 + ("\ntest_size = " + Double.valueOf(percentageTest)));
+      String _pandasCode_7 = pandasCode;
+      pandasCode = (_pandasCode_7 + "\nX_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size) ");
+      String _pandasCode_8 = pandasCode;
+      pandasCode = (_pandasCode_8 + ("\n" + algorithmBody));
+      for (final ValidationMetric validationMetric_1 : metrics) {
+        {
+          String _pandasCode_9 = pandasCode;
+          String _string_1 = validationMetric_1.getLiteral().toString();
+          String _plus_2 = ("\naccuracy = " + _string_1);
+          String _plus_3 = (_plus_2 + "(y_test, y_pred)");
+          pandasCode = (_pandasCode_9 + _plus_3);
+          String _pandasCode_10 = pandasCode;
+          String _string_2 = validationMetric_1.getLiteral().toString();
+          String _plus_4 = ("\nprint(\'" + _string_2);
+          String _plus_5 = (_plus_4 + ":\', accuracy)");
+          pandasCode = (_pandasCode_10 + _plus_5);
+        }
+      }
+      byte[] _bytes = pandasCode.getBytes();
+      File _file = new File("mml.py");
+      Files.write(_bytes, _file);
+      final Process p = Runtime.getRuntime().exec("python mml.py");
+      InputStream _inputStream = p.getInputStream();
+      InputStreamReader _inputStreamReader = new InputStreamReader(_inputStream);
+      final BufferedReader in = new BufferedReader(_inputStreamReader);
+      String line = null;
+      while (((line = in.readLine()) != null)) {
+        System.out.println(line);
+      }
+      return pandasCode;
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 }
