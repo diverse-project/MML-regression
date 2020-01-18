5372ee4400f97e780fc1f2dde701cbfda7d7d6b1
MAZAR Abdellah <bi.douz95@gmail.com>
2020-01-18 20:10:02 +0100

    add Output class

---
+++ org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/bobo_abdellah_sabrina_charaf/Output.xtend
@@ -0,0 +1,16 @@
+package org.xtext.example.mydsl.tests.bobo_abdellah_sabrina_charaf
+
+import org.xtext.example.mydsl.mml.FrameworkLang
+import org.xtext.example.mydsl.mml.MLAlgorithm
+import org.xtext.example.mydsl.mml.ValidationMetric
+
+class Output {
+	
+	MLAlgorithm mlAlgorithm;
+	FrameworkLang frameworkLang;
+	String fileLocation;
+	double result;
+	double timestamp;
+	ValidationMetric validationMetric;
+	
+}
---
+++ org.xtext.example.mml.tests/xtend-gen/org/xtext/example/mydsl/tests/bobo_abdellah_sabrina_charaf/Output.java
@@ -0,0 +1,20 @@
+package org.xtext.example.mydsl.tests.bobo_abdellah_sabrina_charaf;
+
+import org.xtext.example.mydsl.mml.FrameworkLang;
+import org.xtext.example.mydsl.mml.MLAlgorithm;
+import org.xtext.example.mydsl.mml.ValidationMetric;
+
+@SuppressWarnings("all")
+public class Output {
+  private MLAlgorithm mlAlgorithm;
+  
+  private FrameworkLang frameworkLang;
+  
+  private String fileLocation;
+  
+  private double result;
+  
+  private double timestamp;
+  
+  private ValidationMetric validationMetric;
+}
